0976d5377e25ac8baaca1be1a43d9530dc02468e
Juhun <wngns8334@gmail.com>
2023-07-11 04:26:29 +0900

    RF-121 UIColor extension 코드 변경

--- ReFree/Source/Util/Extension/UIColor+ReFreeColor.swift
+++ ReFree/Source/Util/Extension/UIColor+ReFreeColor.swift
@@ -12,20 +12,20 @@ extension UIColor {
     
     struct ReFreeColor {
         // Background Colors
-        let background1 = UIColor(named: "Background1")
-        let background2 = UIColor(named: "Background2")
-        let background3 = UIColor(named: "Background3")
-        let background4 = UIColor(named: "Background4")
+        let background1 = UIColor(named: "Background1") ?? .white
+        let background2 = UIColor(named: "Background2") ?? .white
+        let background3 = UIColor(named: "Background3") ?? .white
+        let background4 = UIColor(named: "Background4") ?? .white
         
         // Point Colors
-        let border1 = UIColor(named: "Border1")
-        let border2 = UIColor(named: "Border2")
-        let defaultBack = UIColor(named: "DefaultBack")
-        let recipeBack1 = UIColor(named: "RecipeBack1")
-        let recipeBack2 = UIColor(named: "RecipeBack2")
-        let selectedBack = UIColor(named: "SelectedBack")
-        let sideBarIcon = UIColor(named: "SideBarIcon")
-        let unSelectedBack = UIColor(named: "UnSelectedBack") // == White
-        let unSelectedIcon = UIColor(named: "UnSelectedIcon")
+        let border1 = UIColor(named: "Border1") ?? .white
+        let border2 = UIColor(named: "Border2") ?? .white
+        let defaultBack = UIColor(named: "DefaultBack") ?? .white
+        let recipeBack1 = UIColor(named: "RecipeBack1") ?? .white
+        let recipeBack2 = UIColor(named: "RecipeBack2") ?? .white
+        let selectedBack = UIColor(named: "SelectedBack") ?? .white
+        let sideBarIcon = UIColor(named: "SideBarIcon") ?? .white
+        let unSelectedBack = UIColor(named: "UnSelectedBack") ?? .white // == White
+        let unSelectedIcon = UIColor(named: "UnSelectedIcon") ?? .white
     }
 }
