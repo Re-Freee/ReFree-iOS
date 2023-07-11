3a0d6e8b8607b8508ac7c7b3ef46772b6aa5c9a6
Juhun <wngns8334@gmail.com>
2023-07-12 02:45:05 +0900

    누락 아이콘 추가

---
+++ ReFree/Source/Recipe/View/RecipeSidebarView.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeSidebarView.swift
+//  ReFree
+//
+//  Created by 이주훈 on 2023/07/12.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class RecipeSidebarView: UIView {
+    
+}
