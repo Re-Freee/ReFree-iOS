c3a5d6cfa3e01d15a25aaa84a97f20ffd86ad035
Juhun <wngns8334@gmail.com>
2023-07-12 03:42:29 +0900

    RF-58 레시피 사이드 바 기본 UI 구현

--- ReFree/Source/Recipe/View/FoodKindView.swift
+++ ReFree/Source/Recipe/View/FoodKindView.swift
@@ -10,24 +10,62 @@ import SnapKit
 import Then
 
 final class FoodKindView: UIView {
-    enum Kind {
-        case bowl
-        case soup
-        case sideMenu
-        case dessert
-        case save
+    enum Kind: String {
+        case bowl = "bowl"
+        case soup = "soup"
+        case sideMenu = "side"
+        case dessert = "dessert"
+        case save = "saveHeart"
+        
+        var foodScript: String {
+            switch self {
+            case .bowl: return "밥 레시피"
+            case .soup: return "국&찌개 레시피"
+            case .sideMenu: return "반찬 레시피"
+            case .dessert: return "후식 레시피"
+            case .save: return "저장한 레시피"
+            }
+        }
     }
     
-    private let imageView = UIImageView(
-        image: UIImage(named: "")
-    )
+    private let title = UILabel() // TODO: Font
+    private let imageView = UIImageView()
+    
+    var kind: Kind
     
     init(kind: Kind) {
+        self.kind = kind
         super.init(frame: .zero)
-        
+        config()
+        layout()
     }
     
-    required init?(coder: NSCoder) {
+    required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func config() {
+        imageView.image = UIImage(named: kind.rawValue)
+        title.text = kind.foodScript
+        addSubviews([imageView, title])
+    }
+
+    private func layout() {
+        imageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24)
+            $0.centerY.equalToSuperview()
+            $0.height.width.equalTo(20)
+        }
+        
+        title.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(12)
+            $0.leading.equalTo(imageView.snp.trailing).offset(24)
+        }
+        
+        snp.makeConstraints {
+            $0.height.equalTo(60)
+        }
+        
+        layer.cornerRadius = 15
     }
 }
