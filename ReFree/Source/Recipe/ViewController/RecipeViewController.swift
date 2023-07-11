ae8d6cc0868e7302565398a0735966034af1e480
Juhun <wngns8334@gmail.com>
2023-07-12 02:14:00 +0900

    RF-56 RF=57 Pagenation 애니메이션 구현 및 Page Control 추가

--- ReFree/Source/Recipe/ViewController/RecipeViewController.swift
+++ ReFree/Source/Recipe/ViewController/RecipeViewController.swift
@@ -11,16 +11,16 @@ import Then
 
 final class RecipeViewController: UIViewController {
     private enum Const {
-       static let itemSize = CGSize(width: 250, height: 400)
-       static let itemSpacing = 30.0
-       
-       static var insetX: CGFloat {
-         (UIScreen.main.bounds.width - Self.itemSize.width) / 2.0
-       }
-       static var collectionViewContentInset: UIEdgeInsets {
-         UIEdgeInsets(top: 0, left: Self.insetX, bottom: 0, right: Self.insetX)
-       }
-     }
+        static let itemSize = CGSize(width: 250, height: 400)
+        static let itemSpacing = 30.0
+        
+        static var insetX: CGFloat {
+            (UIScreen.main.bounds.width - Self.itemSize.width) / 2.0
+        }
+        static var collectionViewContentInset: UIEdgeInsets {
+            UIEdgeInsets(top: 0, left: Self.insetX, bottom: 0, right: Self.insetX)
+        }
+    }
     private let header = RecipeTabHeader(frame: .zero)
     
     private lazy var carouselCollectionView = UICollectionView(
@@ -37,7 +37,7 @@ final class RecipeViewController: UIViewController {
         $0.contentInset = Const.collectionViewContentInset
         $0.decelerationRate = .fast
         $0.translatesAutoresizingMaskIntoConstraints = false
-
+        
         
         $0.register(
             CarouselCell.self,
@@ -53,10 +53,15 @@ final class RecipeViewController: UIViewController {
     }
     
     private let pageControl = UIPageControl().then {
-        $0.backgroundColor = .black
-        // TODO: 나중에 설정하자
+        $0.layer.cornerRadius = 15
+        $0.backgroundColor = .refreeColor.selectedBack
+        $0.pageIndicatorTintColor = .refreeColor.background3
+        $0.currentPageIndicatorTintColor = .refreeColor.unSelectedIcon
+        // TODO: ViewModel과 함께 설정
         $0.numberOfPages = 3
     }
+    
+    private var previousIndex: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -82,9 +87,10 @@ final class RecipeViewController: UIViewController {
         }
         
         pageControl.snp.makeConstraints {
-            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.centerX.equalToSuperview()
             $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(24)
             $0.height.equalTo(30)
+//            $0.width.equalTo(100)
             $0.top.equalTo(carouselCollectionView.snp.bottom)
         }
     }
@@ -96,19 +102,41 @@ final class RecipeViewController: UIViewController {
 }
 
 extension RecipeViewController: UICollectionViewDelegateFlowLayout {
-  func scrollViewWillEndDragging(
-    _ scrollView: UIScrollView,
-    withVelocity velocity: CGPoint,
-    targetContentOffset: UnsafeMutablePointer<CGPoint>
-  ) {
-    let scrolledOffsetX = targetContentOffset.pointee.x + scrollView.contentInset.left
-    let cellWidth = Const.itemSize.width + Const.itemSpacing
-    let index = round(scrolledOffsetX / cellWidth)
-    targetContentOffset.pointee = CGPoint(
-        x: index * cellWidth - scrollView.contentInset.left,
-        y: scrollView.contentInset.top
-    )
-  }
+    func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
+        let scrolledOffsetX = targetContentOffset.pointee.x + scrollView.contentInset.left
+        let cellWidth = Const.itemSize.width + Const.itemSpacing
+        let index = round(scrolledOffsetX / cellWidth)
+        targetContentOffset.pointee = CGPoint(
+            x: index * cellWidth - scrollView.contentInset.left,
+            y: scrollView.contentInset.top
+        )
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrolledOffset = scrollView.contentOffset.x + scrollView.contentInset.left
+        let cellWidth = Const.itemSize.width + Const.itemSpacing
+        let index = Int(round(scrolledOffset / cellWidth))
+        
+        guard
+            let previousIndex,
+            previousIndex != index
+        else { return }
+        
+        let prevCell = carouselCollectionView.cellForItem(at: IndexPath(row: previousIndex, section: 0))
+        let currentCell = carouselCollectionView.cellForItem(at: IndexPath(row: index, section: 0))
+        
+        UIView.animate(withDuration: 0.5) {
+            prevCell?.transform = CGAffineTransform(scaleX: 1, y: 1)
+            currentCell?.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }
+        
+        self.previousIndex = index
+        pageControl.currentPage = index
+    }
 }
 
 extension RecipeViewController: UICollectionViewDataSource {
@@ -127,6 +155,14 @@ extension RecipeViewController: UICollectionViewDataSource {
             withReuseIdentifier: CarouselCell.identifier,
             for: indexPath
         ) as? CarouselCell else { return UICollectionViewCell() }
+        
+        if previousIndex == nil {
+            UIView.animate(withDuration: 1) {
+                cell.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+            }
+            previousIndex = 0
+        }
+        
         cell.setData()
         
         return cell
