927d0be0243d5c54e0286176008969d7ea9bc5c5
Juhun <wngns8334@gmail.com>
2023-07-11 14:42:02 +0900

    RF-35 검색 Header 구현(searchBar 커스텀 필요)

--- ReFree/Source/Common/View/ViewController.swift
+++ ReFree/Source/Common/View/ViewController.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    let headerView = RecipeTabHeader(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.gradientBackground(type: .mainConic)
+        view.gradientBackground(type: .mainAxial)
+        
+        view.addSubview(headerView)
+        
+        headerView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(Constant.spacing24)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
---
+++ ReFree/Source/Util/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  ReFree
+//
+//  Created by 이주훈 on 2023/07/11.
+//
+
+import Foundation
+
+struct Constant {
+    static let spacing12: Int = 12
+    static let spacing24: Int = 24
+    static let spacing50: Int = 50
+    static let spacing60: Int = 60
+}
