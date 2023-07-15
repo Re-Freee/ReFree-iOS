65ee49823417d5dfa72745ef55b73b2ea34a4f84
Juhun <wngns8334@gmail.com>
2023-07-16 00:39:25 +0900

    RF-165 xcconfig 앱 이름 변경

--- ReFree/Source/Util/KeyChain.swift
+++ ReFree/Source/Util/KeyChain.swift
@@ -5,27 +5,27 @@
 //  Created by 이주훈 on 2023/07/15.
 //
 
-import Foundation
-
-struct KeyChain {
-    struct AccessToken {
-        let token: String
-    }
-    
-    enum KeyChainError: Error {
-        case encodingFailed
-    }
-    
-    static let shared = KeyChain()
-    
-    private init() {}
-    
-    func addToken(accessToken: KeyChain.AccessToken) throws {
-        guard let token = accessToken.token.data(using: .utf8) else { throw KeyChainError.encodingFailed}
-        var query: [String: Any] = [
-            kSecClass as String: kSecClassCertificate,
-//            kSecAttrServer as String: ReFreeServer,
-            kSecValueData as String: token
-        ]
-    }
-}
+//import Foundation
+//
+//struct KeyChain {
+//    struct AccessToken {
+//        let token: String
+//    }
+//    
+//    enum KeyChainError: Error {
+//        case encodingFailed
+//    }
+//    
+//    static let shared = KeyChain()
+//    
+//    private init() {}
+//    
+//    func addToken(accessToken: KeyChain.AccessToken) throws {
+//        guard let token = accessToken.token.data(using: .utf8) else { throw KeyChainError.encodingFailed}
+//        var query: [String: Any] = [
+//            kSecClass as String: kSecClassCertificate,
+////            kSecAttrServer as String: ReFreeServer,
+//            kSecValueData as String: token
+//        ]
+//    }
+//}
