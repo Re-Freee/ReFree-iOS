b2245dce51861cc66333e8c348b2ca9ed21a460d
Juhun <wngns8334@gmail.com>
2023-07-17 14:39:58 +0900

    RF-66 RF-67  동적 레이아웃 적용
    CollectionView FlowLayout -> Compositional Layout으로 동적 크기 및 레이아웃 수정

--- ReFree/Source/Common/View/RFModal/RecipeDetailHeaderView.swift
+++ ReFree/Source/Common/View/RFModal/RecipeDetailHeaderView.swift
@@ -34,11 +34,17 @@ final class RecipeDetailHeaderView: UICollectionReusableView, Identifiable {
     }
     
     private let ingredientsLists = UILabel().then {
-        $0.numberOfLines = 5
+        $0.numberOfLines = 0
         $0.textColor = .refreeColor.text1
         $0.font = .pretendard.extraLight12
-        // TODO: Remove
-        $0.text = "물(250ml 기준) 구매2컵. 돼지고기 찌개용 또는 목살 구매250g. 신김치 구매200g. 김칫국물 구매5큰술. 참기름 구매1작은술. 양파 구매1/2개. 청고추 구매2개. 대파"
+        // TODO: 테스트 용 긴 문자열로 실제 정보 받을땐 제거 예정
+        $0.text = "물(250ml 기준) 구매2컵. 돼지고기 찌개용 또는 목살 구매250g. 신김치 구매200g. 김칫국물 구매5큰술. 참기름 구매1작은술. 양파 구매1/2개. 청고추 구매2개. 대파물(250ml 기준) 구매2컵. 돼지고기 찌개용 또는 목살 구매250g. 신김치 구매200g. 김칫국물 구매5큰술. 참기름 구매1작은술. 양파 구매1/2개. 청고추 구매2개. 대파물(250ml 기준) 구매2컵. 돼지고기 찌개용 또는 목살 구매250g. 신김치 구매200g. 김칫국물 구매5큰술. 참기름 구매1작은술. 양파 구매1/2개. 청고추 구매2개. 대파물(250ml 기준) 구매2컵. 돼지고기 찌개용 또는 목살 구매250g. 신김치 구매200g. 김칫국물 구매5큰술. 참기름 구매1작은술. 양파 구매1/2개. 청고추 구매2개. 대파물(250ml 기준) 구매2컵. 돼지고기 찌개용 또는 목살 구매250g. 신김치 구매200g. 김칫국물 구매5큰술. 참기름 구매1작은술. 양파 구매1/2개. 청고추 구매2개. 대파물(250ml 기준) 구매2컵. 돼지고기 찌개용 또는 목살 구매250g. 신김치 구매200g. 김칫국물 구매5큰술. 참기름 구매1작은술. 양파 구매1/2개. 청고추 구매2개. 대파"
+    }
+    
+    private let recipeTitle = UILabel().then {
+        $0.font = .pretendard.bold20
+        $0.textColor = .refreeColor.main
+        $0.text = "조리순서"
     }
     
     private var isBookmarked: Bool = false {
@@ -73,7 +79,8 @@ final class RecipeDetailHeaderView: UICollectionReusableView, Identifiable {
             titleLabel,
             bookmarkButton,
             ingredientTitle,
-            ingredientsLists
+            ingredientsLists,
+            recipeTitle
         ])
         
         titleLabel.snp.makeConstraints {
@@ -88,13 +95,19 @@ final class RecipeDetailHeaderView: UICollectionReusableView, Identifiable {
         
         ingredientTitle.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(24)
-            $0.leading.trailing.equalToSuperview().offset(24)
+            $0.leading.trailing.equalToSuperview()
         }
         
         ingredientsLists.snp.makeConstraints {
             $0.top.equalTo(ingredientTitle.snp.bottom).offset(12)
             $0.leading.equalToSuperview().offset(24)
             $0.trailing.equalToSuperview().offset(-24)
+        }
+        
+        recipeTitle.snp.makeConstraints {
+            $0.top.equalTo(ingredientsLists.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-12)
         }
     }
 }
