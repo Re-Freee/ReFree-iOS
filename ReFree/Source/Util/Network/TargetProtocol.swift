3aa401b83c809f26798fe221d09708dfa82fdb39
Juhun <wngns8334@gmail.com>
2023-07-23 02:36:31 +0900

    RF-193 get 쿼리 요청 메서드 추가

--- ReFree/Source/Util/Network/TargetProtocol.swift
+++ ReFree/Source/Util/Network/TargetProtocol.swift
@@ -24,3 +24,28 @@ protocol Target {
     var encoding: ParameterEncoding { get }
     var images: [UIImage] { get }
 }
+
+struct GetQuery {
+    let key: String
+    let value: Any
+    
+    init(_ key: String,_ value: Any) {
+        self.key = key
+        self.value = value
+    }
+}
+
+extension Target {
+    func setQuery(url: String, query: [GetQuery]?) -> String{
+        guard let query else { return url }
+        var url = url
+        query.forEach {
+            url.append("\($0.key)=\($0.value)&")
+        }
+        if let last = url.last,
+           last == "&" {
+            url.removeLast()
+        }
+        return url
+    }
+}
