9f9c38d91e52581f3608bc0de29931d7d67016fc
HyungSuk Kim <kimhyungsuk982@gmail.com>
2023-07-27 19:37:43 +0900

    [RF-41] 홈 화면 TableView 추가

--- ReFree/Source/Home/ViewController/HomeViewController.swift
+++ ReFree/Source/Home/ViewController/HomeViewController.swift
@@ -6,9 +6,26 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate {
     private let header = HomeTabHeader(frame: .zero)
+    
+    private lazy var foodTableView = UITableView(
+        frame: .zero,
+        style: .plain
+    ).then {
+//        $0.clipsToBounds = false
+        $0.separatorStyle = .none
+        $0.backgroundColor = .clear
+        $0.rowHeight = 100
+        
+        $0.register(
+            FoodTableViewCell.self,
+            forCellReuseIdentifier: FoodTableViewCell.identifier
+        )
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,14 +35,66 @@ class HomeViewController: UIViewController {
     private func config() {
         view.gradientBackground(type: .mainAxial)
         layout()
+        foodTableView.delegate = self
+        foodTableView.dataSource = self
     }
     
     private func layout() {
-        view.addSubview(header)
+        view.addSubviews(
+            [
+                header,
+                foodTableView
+            ]
+        )
         
-        header.snp.makeConstraints { make in
-            make.top.leading.bottom.trailing.equalTo(view.safeAreaLayoutGuide)
+        header.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(160)
+        }
+        
+        foodTableView.snp.makeConstraints {
+            $0.top.equalTo(header.snp.bottom)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(24)
         }
     }
 
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let spacingFooterView = UIView()
+        spacingFooterView.backgroundColor = .clear
+        return spacingFooterView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: FoodTableViewCell.identifier,
+            for: indexPath
+        ) as? FoodTableViewCell else { return UITableViewCell() }
+        
+        cell.setData()
+        cell.selectionStyle = .none
+        
+        return cell
+    }
 }
