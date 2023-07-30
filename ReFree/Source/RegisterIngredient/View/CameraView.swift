d54963ae1e2ef575fb629312df01753fff67ae8b
Juhun <wngns8334@gmail.com>
2023-07-30 18:47:21 +0900

    fix: 카메라 이미지 적용 수정

--- ReFree/Source/RegisterIngredient/View/CameraView.swift
+++ ReFree/Source/RegisterIngredient/View/CameraView.swift
@@ -10,11 +10,15 @@ import SnapKit
 import Then
 
 final class CameraView: UIView {
-    let imageView = UIImageView(image: UIImage(named: "Camera1")).then {
+    private let imageView = UIImageView(image: UIImage(named: "Camera1")).then {
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 15
         $0.backgroundColor = .refreeColor.text1
         $0.contentMode = .center
+    }
+    
+    var currentImage: UIImage? {
+        return imageView.image
     }
     
     init() {
@@ -33,4 +37,17 @@ final class CameraView: UIView {
             $0.edges.equalToSuperview()
         }
     }
+    
+    func setImage(image: UIImage) {
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .clear
+    }
+    
+    func setDefault() {
+        imageView.image = UIImage(named: "Camera1")
+        imageView.backgroundColor = .refreeColor.text1
+        imageView.contentMode = .center
+    }
+
 }
