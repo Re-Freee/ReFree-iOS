d8ab3dc33df7c2cafba657fb4b8520ba6e66b243
Juhun <wngns8334@gmail.com>
2023-08-09 02:33:29 +0900

    test

---
+++ ReFree/Source/Util/Extension/Date+String.swift
@@ -0,0 +1,16 @@
+//
+//  Date+String.swift
+//  ReFree
+//
+//  Created by 이주훈 on 2023/08/09.
+//
+
+import Foundation
+
+extension Date {
+    func toString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.mm.dd"
+        return dateFormatter.string(from: self)
+    }
+}
